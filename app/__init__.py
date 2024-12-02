5ab13173128ea7a73729f92eede665c765df03af
pedro <pedro092692@gmail.com>
2024-12-02 17:30:17 +0100

    add bootstrap plugin

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
+from extensions import Bootstrap5
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    # plugins
+    bootstrap = Bootstrap5(app)
 
     # blueprints
     from app.main import bp as main_bp
--- extensions.py
+++ extensions.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin, login_required, login_user, logout_user, LoginManager, current_user
+from flask_bootstrap import Bootstrap5
 
 login_manager = LoginManager()
 login_manager.login_view = "security.login"
