ce79eb7f0dae3a630e14cc84ac29a0f6e86edeac
pedro <pedro092692@gmail.com>
2024-12-02 18:20:11 +0100

    create get_user_id method

--- app/models/user.py
+++ app/models/user.py
@@ -7,3 +7,9 @@ class User:
     email = db.Column(db.String(100))
     password = db.Column(db.String(200))
     role = db.Column(db.String(20))
+
+    @staticmethod
+    def get_user_id(user_id):
+        user = db.session.execute(db.select(User).filter(User.id == user_id)).scalar()
+        return user
+
