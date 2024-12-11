bc7065381750396087767bcf881ba8f356b571ac
pedro <pedro092692@gmail.com>
2024-12-11 23:32:37 +0100

    create work card styles

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,10 @@
 from flask import render_template, redirect
+from app.models.works import Work
 from app.main import bp
 
 
 @bp.route('/', methods=['GET'])
 def home():
-    return render_template('main/index.html')
+    works = Work.get_works()
+    return render_template('main/index.html', works=works)
 
